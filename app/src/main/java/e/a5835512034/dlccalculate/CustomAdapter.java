06319851cf7445d5142ef053078f439495d13809
samchampkittisak <5835512034@email.psu.ac.th>
2018-04-17 02:37:09 +0700

    v2.00_DLC

---
+++ app/src/main/java/e/a5835512034/dlccalculate/CustomAdapter.java
@@ -0,0 +1,8 @@
+package e.a5835512034.dlccalculate;
+
+/**
+ * Created by 5835512034 on 4/16/2018.
+ */
+
+public class CustomAdapter {
+}
---
+++ app/src/main/java/e/a5835512034/dlccalculate/ItemClickListener.java
@@ -0,0 +1,8 @@
+package e.a5835512034.dlccalculate;
+
+/**
+ * Created by 5835512034 on 4/17/2018.
+ */
+
+public class ItemclickListener {
+}
